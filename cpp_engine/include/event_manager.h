ec28bd88dc68852721ff9da8565a994a37dfacac
parishree-gupta <parishree1906@gmail.com>
2026-05-30 22:30:08 +0530

    updated eventmanager.h

--- cpp_engine/include/event_manager.h
+++ cpp_engine/include/event_manager.h
@@ -1,42 +1,26 @@
 #ifndef EVENT_MANAGER_H
 #define EVENT_MANAGER_H
-
 #include "graph.h"
 #include "models.h"
-
 #include <vector>
 #include <string>
-
 using namespace std;
-
 // ---------- EVENT ----------
 struct Event {
-
     string type;
-
     string description;
-
     int severity;
 };
-
 class EventManager {
-
 private:
-
     Graph& graph;
-
     vector<Event> events;
-
 public:
-
     // constructor
     EventManager(Graph& g);
-
     // generate random events
     void generateEvents();
-
     // display events
     void showEvents();
 };
-
 #endif
