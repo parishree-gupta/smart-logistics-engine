76317efed8f6b9b530d78561f486333d1ba7d7c0
parishree-gupta <parishree1906@gmail.com>
2026-05-30 22:31:00 +0530

    updated graph.h

--- cpp_engine/include/graph.h
+++ cpp_engine/include/graph.h
@@ -1,33 +1,21 @@
 #ifndef GRAPH_H
 #define GRAPH_H
-
 #include <vector>
 #include <string>
-
 #include "models.h"
-
 using namespace std;
-
 class Graph {
-
 private:
-
     int n;
-
     // advanced adjacency list
     vector<vector<Road>> adj;
-
     // city names
     vector<string> cityNames;
-
 public:
-
     // constructor
     Graph(int vertices);
-
     // set city name
     void setCityName(int index, string name);
-
     // add advanced road
     void addEdge(
         int from,
@@ -37,18 +25,13 @@ public:
         bool blocked,
         double fuelCostMultiplier
     );
-
     // shortest path
     vector<int> dijkstra(int src);
-
     // block road dynamically
     void blockRoad(int from, int to);
-
     // getters
     int getVertices();
-
     string getCityName(int index);
-
     vector<vector<Road>> getAdjacencyList();
 };
 
