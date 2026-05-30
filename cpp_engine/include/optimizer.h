d73e0140a43fe9691f267b47b58d6ab0d8258ce4
parishree-gupta <parishree1906@gmail.com>
2026-05-30 22:32:49 +0530

    updated optimizer.h

--- cpp_engine/include/optimizer.h
+++ cpp_engine/include/optimizer.h
@@ -1,36 +1,25 @@
 #ifndef OPTIMIZER_H
 #define OPTIMIZER_H
-
 #include "graph.h"
 #include "models.h"
-
 #include <vector>
-
 using namespace std;
-
 class Optimizer {
-
 private:
-
     Graph& graph;
-
 public:
-
     // constructor
     Optimizer(Graph& g);
-
     // greedy assignment
     int greedyAssign(
         vector<Agent>& agents,
         vector<Order>& orders
     );
-
     // knapsack optimization
     vector<Order> knapsack(
         vector<Order>& orders,
         int capacity
     );
-
     // tsp routing
     pair<vector<int>, int> tsp(
         vector<int>& nodes
